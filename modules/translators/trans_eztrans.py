1e29185f87fa7d588a1b1b0098ed4221e50c6167
zzzzzzzskyward <zzzzzzzskyward@github.io>
2025-01-13 15:35:22 +0800

    fix type annotation crash in python 3.8

--- modules/translators/trans_eztrans.py
+++ modules/translators/trans_eztrans.py
@@ -13,7 +13,7 @@ class MyClient(Client64):
                                        engine_type=engine_type,
                                        dat_path=dat_path)
 
-    def translate(self, src_text: str | list):
+    def translate(self, src_text: "str | list"):
         return self.request32('translate', src_text)
 
 
